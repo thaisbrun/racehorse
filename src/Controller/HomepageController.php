e4a405fb8c87425dd8f8ecc773822952babbdec8
Thaïs Brun <travailthais@outlook.fr>
2023-03-25 16:31:33 +0100

    avancement 25/03

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Annonce;
+use App\Entity\Image;
 use App\Repository\MyClassRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
--- src/Entity/Annonce.php
+++ src/Entity/Annonce.php
@@ -22,7 +22,6 @@ class Annonce
      * @ORM\GeneratedValue(strategy="IDENTITY")
      */
     private $idannonce;
-
     /**
      * @var string
      *
