c7674d51045b7ffccb38cef765be4164d5b5c817
Thaïs Brun <travailthais@outlook.fr>
2023-09-05 22:26:26 +0200

    pleins de progrès et d'ajout de fonctionnalités

---
+++ src/Repository/DepartementRepository.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Departement;
+use App\Entity\Robe;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<Departement>
+ *
+ * @method Departement|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Departement|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Departement[]    findAll()
+ * @method Departement[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class DepartementRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Departement::class);
+    }
+    public function findByIdDepEquide(Departement $idDepartementEquide): ?Departement
+    {
+        $qb = $this->createQueryBuilder('d');
+        //SELECT * from race innerjoin equide on equide.race = race.id where race = r.id;
+        $qb->select('d')
+            //   ->innerJoin('r.id', 'e', 'WITH', 'e.race')
+            ->andWhere('d.iddepartement = :iddep')
+            ->setParameter('iddep', $idDepartementEquide)
+            ->setMaxResults(1);
+        // return equide
+        return $query = $qb->getQuery()->getOneOrNullResult();
+
+    }
+
+//    /**
+//     * @return Departement[] Returns an array of Departement objects
+//     */
+//    public function findByExampleField($value): array
+//    {
+//        return $this->createQueryBuilder('d')
+//            ->andWhere('d.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->orderBy('d.id', 'ASC')
+//            ->setMaxResults(10)
+//            ->getQuery()
+//            ->getResult()
+//        ;
+//    }
+
+//    public function findOneBySomeField($value): ?Departement
+//    {
+//        return $this->createQueryBuilder('d')
+//            ->andWhere('d.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->getQuery()
+//            ->getOneOrNullResult()
+//        ;
+//    }
+}
---
+++ src/Repository/RegionRepository.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Region;
+use App\Entity\Robe;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<Region>
+ *
+ * @method Region|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Region|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Region[]    findAll()
+ * @method Region[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class RegionRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Region::class);
+    }
+
+    public function findByIdDepartement(Region $idDepartement): ?Region
+    {
+        $qb = $this->createQueryBuilder('reg');
+        //SELECT * from race innerjoin equide on equide.race = race.id where race = r.id;
+        $qb->select('reg')
+            //   ->innerJoin('r.id', 'e', 'WITH', 'e.race')
+            ->andWhere('reg.idregion = :idregiondep')
+            ->setParameter('idregiondep', $idDepartement)
+            ->setMaxResults(1);
+        // return equide
+        return $query = $qb->getQuery()->getOneOrNullResult();
+
+    }
+
+//    /**
+//     * @return Region[] Returns an array of Region objects
+//     */
+//    public function findByExampleField($value): array
+//    {
+//        return $this->createQueryBuilder('r')
+//            ->andWhere('r.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->orderBy('r.id', 'ASC')
+//            ->setMaxResults(10)
+//            ->getQuery()
+//            ->getResult()
+//        ;
+//    }
+
+//    public function findOneBySomeField($value): ?Region
+//    {
+//        return $this->createQueryBuilder('r')
+//            ->andWhere('r.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->getQuery()
+//            ->getOneOrNullResult()
+//        ;
+//    }
+}
