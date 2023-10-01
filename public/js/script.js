25e94fbdcaf25bd87fdc4132d02537475452a939
Thaïs Brun <travailthais@outlook.fr>
2023-10-01 13:24:02 +0200

    requetes AJAX

--- public/js/script.js
+++ public/js/script.js
@@ -2,11 +2,16 @@ function closeMessage(){
         document.getElementById("messagePrev").style.display = "none";
 }
 window.onload = () => {
-        const filtersForm = document.querySelector("#filters");
+        const FiltersForm = document.querySelector("#filters");
 
         document.querySelectorAll("#filters input").forEach(input =>{
            input.addEventListener("change", () =>{
-                   console.log("clic");
+               const Form = new FormData(FiltersForm);
+               const Params = new URLSearchParams();
+                   Form.forEach((value, key) =>{
+                       Params.append(key,value);
+                       console.log(Params.toString());
+                   });
            })
         });
 }
