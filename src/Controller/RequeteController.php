4b92b015b039e0c0abfca59757e00be910bc3919
Thaïs Brun <travailthais@outlook.fr>
2024-05-20 11:26:01 +0200

    modif du 20/05

--- src/Controller/RequeteController.php
+++ src/Controller/RequeteController.php
@@ -4,8 +4,6 @@ namespace App\Controller;
 
 use App\Entity\Requete;
 use App\Form\RequeteType;
-use App\Repository\AnnonceRepository;
-use App\Repository\RequeteRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,28 +2,18 @@
 
 namespace App\Controller;
 
-use App\Entity\Annonce;
-use App\Entity\Favoris;
+
 use App\Entity\Utilisateur;
-use App\Form\AnnonceType;
-use App\Form\RegistrationFormType;
-use App\Form\ResetPasswordRequestFormType;
 use App\Form\UtilisateurType;
 use App\Repository\AnnonceRepository;
 use App\Repository\FavorisRepository;
 use App\Repository\UtilisateurRepository;
-use App\Security\AuthAuthenticator;
-use Doctrine\ORM\EntityManagerInterface;
-use Monolog\Handler\Curl\Util;
+use LogicException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Security\Csrf\TokenGenerator\TokenGeneratorInterface;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
-use Symfony\Component\Security\Http\Authentication\UserAuthenticatorInterface;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 
 //Ce controller est lié à la page de connexion et de profil, expliquant les informations principales de l'utilisateur.
 class SecurityController extends AbstractController
@@ -99,9 +89,8 @@ class SecurityController extends AbstractController
         $lastUsername = $authenticationUtils->getLastUsername();
         return $this->render('security/viewProfil.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
     }
-    #[Route(path: 'security/forgottenPassword', name: 'app_security_forgottenpassword')]
-    public function forgottenPassword(Request $request, UtilisateurRepository $utilisateurRepository,
-    TokenGeneratorInterface $tokenGenerator, EntityManagerInterface $entityManager): Response
+ /*   #[Route(path: 'security/forgottenPassword', name: 'app_security_forgottenpassword')]
+    public function forgottenPassword(Request $request, UtilisateurRepository $utilisateurRepository): Response
     {
         $formPassword = $this->createForm(ResetPasswordRequestFormType::class);
         $formPassword->handleRequest($request);
@@ -117,23 +106,22 @@ class SecurityController extends AbstractController
                 'formPassword' => $formPassword->createView(),
             ]);
 
-    }
+    } */
     #[Route(path: 'security/logout', name: 'app_security_logout')]
     public function logout(): void
     {
-        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+        throw new LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
     #[Route(path:'security/delete/{idutilisateur}', name: 'app_security_delete', methods: ['GET','POST'])]
     public function delete(Utilisateur $user, UtilisateurRepository $utilisateurRepository): Response
     {
         if($this->getUser() !== $user || $this->getUser() == null) {
             $this->addFlash('error', "Accès non autorisé");
-            return $this->redirectToRoute('homepage', [], Response::HTTP_SEE_OTHER);
         } else {
+            $this->addFlash('success', "Compte supprimé avec succès");
             $utilisateurRepository->remove($user, true);
-
-            return $this->redirectToRoute('homepage', [], Response::HTTP_SEE_OTHER);
         }
+        return $this->redirectToRoute('homepage', [], Response::HTTP_SEE_OTHER);
     }
 
 }
